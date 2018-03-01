d526007d87e888c16cb9130c12c1ee3e9a05a467
bookmad <shihying3@gamil.com>
2018-03-01 15:04:37 +0800

    Site updated: 2018-03-01 15:04:35

---
+++ resume/en/js/lang.js
@@ -0,0 +1,19 @@
+var langs = ['en', 'zh'];
+var langCode = '';
+var langJS = null;
+
+
+var translate = function (jsdata)
+{	
+	$("[tkey]").each (function (index)
+	{
+		var strTr = jsdata [$(this).attr ('tkey')];
+	    $(this).html (strTr);
+	});
+}
+
+
+
+
+
+
